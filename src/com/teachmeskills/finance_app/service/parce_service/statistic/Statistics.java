9946a9aec424dfb581af8769703f62b11cbb561b
Sergey Berdnikov <berdnikausiarhei@gmail.com>
2024-11-10 22:43:41 +0300

    TMC-C32-HM-Course
    1. added parser orders
    2. remove class statistics

--- src/com/teachmeskills/finance_app/service/parce_service/statistic/Statistics.java
+++
@@ -1,9 +0,0 @@
-package com.teachmeskills.finance_app.service.parce_service.statistic;
-
-public class Statistics {
-    private double totalInvoices;
-    private double totalOrders;
-    private double totalReceipts;
-
-    //TODO Методы для получения и установки статистики
-}
