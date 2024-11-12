e7ad5f7ffa8571271e70a66a6b22abe5a7f6f3ac
Sergey <berdnikausiarhei@gmail.com>
2024-11-12 15:31:26 +0300

    TMC-C32-HM-Course
    1. Corrected ApplicationRunner

--- src/com/teachmeskills/finance_app/ApplicationRunner.java
+++ src/com/teachmeskills/finance_app/ApplicationRunner.java
@@ -19,18 +19,23 @@ public class ApplicationRunner {
         String inputPass = scanner.nextLine();
         AuthService authService = new AuthService();
         SessionManager sessionClient1 = authService.auth(inputLogin, inputPass);
-        OrderParserImpl order = new OrderParserImpl();
-        CheckParserImpl check = new CheckParserImpl();
-        InvoiceParserImpl invoice = new InvoiceParserImpl();
-        System.out.print("Введите путь к файлу Ордеров: ");
-        String directoryPathOrder = scanner.nextLine();// src/com/teachmeskills/finance_app/resources/data/orders
-        System.out.print("Введите путь к файлу Чеков: ");
-        String directoryPathCheck = scanner.nextLine();//src/com/teachmeskills/finance_app/resources/data/checks
-        System.out.print("Введите путь к файлу Инвойсов: ");
-        String directoryPathInvoice = scanner.nextLine();  //src/com/teachmeskills/finance_app/resources/data/invoices
-        order.validatorDocument(directoryPathOrder ,sessionClient1);
-        check.validatorDocument(directoryPathCheck, sessionClient1);
-        invoice.validatorDocument(directoryPathInvoice, sessionClient1);
+        if (sessionClient1.isSessionValid()){
+            OrderParserImpl order = new OrderParserImpl();
+            CheckParserImpl check = new CheckParserImpl();
+            InvoiceParserImpl invoice = new InvoiceParserImpl();
+            System.out.print("Введите путь к файлу Ордеров: ");
+            String directoryPathOrder = scanner.nextLine();// src/com/teachmeskills/finance_app/resources/data/orders
+            System.out.print("Введите путь к файлу Чеков: ");
+            String directoryPathCheck = scanner.nextLine();//src/com/teachmeskills/finance_app/resources/data/checks
+            System.out.print("Введите путь к файлу Инвойсов: ");
+            String directoryPathInvoice = scanner.nextLine();  //src/com/teachmeskills/finance_app/resources/data/invoices
+            order.validatorDocument(directoryPathOrder ,sessionClient1);
+            check.validatorDocument(directoryPathCheck, sessionClient1);
+            invoice.validatorDocument(directoryPathInvoice, sessionClient1);
+        }else {
+            LoggerService.logInfo("Сессия не валидная");
+        }
+
     }
 
 }
